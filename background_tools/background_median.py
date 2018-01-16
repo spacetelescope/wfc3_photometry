2c200b9f3563b8684ab0e601ec29bfd3eb6cb1d7
Varun Bajaj <vbajaj@stsci.edu>
2018-01-16 16:48:35 -0500

    Added sigma clipping to background measurements

--- background_tools/background_median.py
+++ background_tools/background_median.py
@@ -2,7 +2,7 @@
 
 The functions in this script enable the computation of statistics
 within a PhotUtils aperture, which is currently not directly
-implemented in PhotUtils itself.  This code is meant to be 
+implemented in PhotUtils itself.  This code is meant to be
 imported into other code, and then be usable as a single line to
 return all the statistics in a format similar to the
 aperture_photometry method in PhotUtils (i.e. an astropy table).
@@ -14,69 +14,81 @@ Use
 ---
     from background_median import aperture_stats_tbl
     stats_tbl = aperture_stats_tbl(data, apertures)
-    
-    See the docstring of aperture_stats_tbl for more info.    
+
+    See the docstring of aperture_stats_tbl for more info.
 """
 import numpy as np
 from astropy.table import Table
+from scipy.stats import sigmaclip # WAY faster than astropy.stats.sigma_clipped_stats
 
-def aperture_stats_tbl(data, apertures, method='exact'):
-    """Computes mean, median, and mode statistics inside Photutils 
-    apertures. This is primarily intended for estimating 
-    backgrounds via annulus apertures.  The intent is that this 
+
+def aperture_stats_tbl(data, apertures, method='exact', sigma_clip=False):
+    """Computes mean, median, and mode statistics inside Photutils
+    apertures. This is primarily intended for estimating
+    backgrounds via annulus apertures.  The intent is that this
     falls easily into other code to provide background measurements.
-    
+
     Parameters
     ----------
     data : array
         The data for the image to be measured.
     apertures : photutils.aperture.core.PixelAperture object (or subclass)
         The phoutils aperture object to measure the stats in.
-        i.e. the object returned via CirularAperture, CircularAnnulus, 
+        i.e. the object returned via CirularAperture, CircularAnnulus,
         or RectangularAperture etc.
     method: str
-        The method by which to handle the pixel overlap.  Defaults to 
+        The method by which to handle the pixel overlap.  Defaults to
         computing the exact area.
-        NOTE: Currently, this will actually fully include a pixel where 
+        NOTE: Currently, this will actually fully include a pixel where
         the aperture has ANY overlap,
-        as a median is also being performed.  If the method is set to 
-        'center' the pixels will only be included if the pixel's center 
+        as a median is also being performed.  If the method is set to
+        'center' the pixels will only be included if the pixel's center
         falls within the aperture.
-        
+    sigma_clip: bool
+        Flag to activate sigma clipping of background pixels
+
     Returns
     -------
     stats_tbl : astropy.table.Table
-        An astropy Table with the colums X, Y, aperture_mean, 
-        aperture_median, aperture_mode,
+        An astropy Table with the colums X, Y, aperture_mean,
+        aperture_median, aperture_mode, aperture_std, aperture_area
         and a row for each of the positions of the apertures.
-    
+
     """
-    
+
     # Get the masks that will be used to identify our desired pixels.
-    masks = apertures.to_mask(method=method) 
+    masks = apertures.to_mask(method=method)
     # Compute the stats of pixels within the masks
-    aperture_stats = [calc_aperture_mmm(data, mask) for mask in masks]
+    aperture_stats = [calc_aperture_mmm(data, mask, sigma_clip) for mask in masks]
     aperture_stats = np.array(aperture_stats)
-    
+
     # Place the array of the x y positions alongside the stats
     stacked = np.hstack([apertures.positions, aperture_stats])
     # Name the columns
-    names = ['X','Y','aperture_mean','aperture_median','aperture_mode']
+    names = ['X','Y','aperture_mean','aperture_median','aperture_mode',
+            'aperture_std', 'aperture_area']
     # Make the table
     stats_tbl = Table(data=stacked, names=names)
     return stats_tbl
 
-def calc_aperture_mmm(data, mask):
-    """Helper function to actually calculate the stats for pixels 
+def calc_aperture_mmm(data, mask, sigma_clip=False):
+    """Helper function to actually calculate the stats for pixels
         falling within some Photutils aperture mask on some array
         of data.
     """
     cutout = mask.cutout(data, fill_value=np.nan)
     if cutout is None:
-        return (np.nan, np.nan, np.nan)
+        return (np.nan, np.nan, np.nan, np.nan, np.nan)
     else:
         values = cutout * mask.data / mask.data
-        mean = np.nanmean(values)
-        median = np.nanmedian(values)
+        values = values[~np.isnan(values)]
+        if sigma_clip:
+            values, clow, chigh = sigmaclip(values, low=3, high=3)
+
+        mean = np.mean(values)
+        median = np.median(values)
+        std = np.std(values)
+
         mode = 3 * median - 2 * mean
-        return (mean, median, mode)
+        actual_area = (~np.isnan(values)).sum()
+        return (mean, median, mode, std, actual_area)
